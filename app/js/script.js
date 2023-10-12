5697fadb868179ea86a1cba027e5ab7a22cca243
Leonid Petrushin <thunderleo@mail.ru>
2023-10-12 19:22:50 +0300

    fux

--- app/js/script.js
+++ app/js/script.js
@@ -1,26 +1,25 @@
 function calculateAverage() {
-    const inputElement = document.getElementById('marks');
-    const outputElement = document.getElementById('output');
-    const averageElement = document.getElementById('average');
-  
-    const inputNumbers = inputElement.value.trim().split(' ');
-    let sum = 0;
-  
-    for (let i = 0; i < inputNumbers.length; i++) {
-      const num = parseFloat(inputNumbers[i]);
-  
-      // Проверка на валидность числа
-      if (!isNaN(num)) {
-        sum += num;
-      }
-    }
-  
-    if (inputNumbers.length > 0) {
-      const average = sum / inputNumbers.length;
-        averageElement.classList.remove("hidden");
-      outputElement.textContent = `${average.toFixed(2)}`;
-    } else {
-      outputElement.textContent = 'Введите допустимые числа.';
+  const inputElement = document.getElementById("marks");
+  const outputElement = document.getElementById("output");
+  const averageElement = document.getElementById("average");
+
+  const inputNumbers = inputElement.value.trim().split(" ");
+  let sum = 0;
+
+  for (let i = 0; i < inputNumbers.length; i++) {
+    const num = parseFloat(inputNumbers[i]);
+
+    // Проверка на валидность числа
+    if (!isNaN(num)) {
+      sum += num;
     }
   }
-  
+
+  if (inputNumbers.length > 0) {
+    averageElement.classList.remove("hidden");
+    const average = sum / inputNumbers.length;
+    outputElement.textContent = `${average.toFixed(2)}`;
+  } else {
+    outputElement.textContent = "Введите допустимые числа.";
+  }
+}
