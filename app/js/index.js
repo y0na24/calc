c197619f79a5a73993a5ed4fce317c8e69b6c98f
Матвей Клёнов <y0na24@mail.ru>
2023-10-13 13:13:01 +0300

    initial commit

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,8 @@
 import './calc.js'
+import { calculateAverage } from './calc.js'
+
+const button = document.querySelector('#calc-btn')
+
+button.addEventListener('click', () => {
+  calculateAverage()
+})
